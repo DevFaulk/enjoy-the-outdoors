60632e91ccdaaac8d517871519106fb576607665
Teddy Faulk <FaulkDev@gmail.com>
2023-11-09 19:33:13 -0500

    grab and identify html elements in javascript

---
+++ scripts/parks.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const byLocationRadioFilter = document.querySelector("#byLocationRadioFilter");
+const byTypeRadioFilter = document.querySelector("#byTypeRadioFilter");
+const locationSelector = document.querySelector("#locationSelector");
+const locationTable = document.querySelector("#locationTable");
+const locationTableBody = document.querySelector("#locationTableBody");

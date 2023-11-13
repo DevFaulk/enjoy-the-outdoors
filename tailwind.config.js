71225e0b71184de032f7402f8584c4542e33028d
DevFaulk <faulkdev@gmail.com>
2023-11-13 10:09:59 -0500

    tailwind savepoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ['./build/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'carousel-item-1': "url('../build/images/item1 carousel.jpg')",
+        'carousel-item-2': "url('../build/images/item2 carousel.jpg')",
+        'carousel-item-3': "url('../build/images/item3 carousel.jpg')",
+      },
+    },
   },
   plugins: [],
 };

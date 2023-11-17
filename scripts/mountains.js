96c6c25ec509f1d16961a1493448fa78d7c5ad1d
DevFaulk <faulkdev@gmail.com>
2023-11-16 20:24:32 -0500

    style mountain page and fix styling elsewhere

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -1,5 +1,5 @@
-const mountainSelect = document.querySelector("#mountainSelect");
-const mountainData = document.querySelector("#mountainData");
+const mountainSelect = document.querySelector('#mountainSelect');
+const mountainData = document.querySelector('#mountainData');
 
 async function displayMountainsInSelect() {
   for (const mountain of mountainsArray) {
@@ -15,13 +15,13 @@ async function displaySunriseTime(lat, lng) {
 
     // Use Moment to format the time in UTC
     const sunriseTime = moment
-      .utc(data.results.sunrise, "HH:mm:ss A")
-      .format("hh:mm A");
+      .utc(data.results.sunrise, 'HH:mm:ss A')
+      .format('hh:mm A');
 
     return sunriseTime;
   } catch (error) {
-    console.error("Error fetching sunrise time:", error);
-    return "N/A";
+    console.error('Error fetching sunrise time:', error);
+    return 'N/A';
   }
 }
 
@@ -31,13 +31,13 @@ async function displaySunsetTime(lat, lng) {
 
     // Use Moment to format the time in UTC
     const sunsetTime = moment
-      .utc(data.results.sunset, "HH:mm:ss A")
-      .format("hh:mm A");
+      .utc(data.results.sunset, 'HH:mm:ss A')
+      .format('hh:mm A');
 
     return sunsetTime;
   } catch (error) {
-    console.error("Error fetching sunset time:", error);
-    return "N/A";
+    console.error('Error fetching sunset time:', error);
+    return 'N/A';
   }
 }
 
@@ -48,85 +48,85 @@ async function displayMountainData() {
   );
 
   if (mountain) {
-    mountainData.innerHTML = ""; // clear previous mountain data
-    let mountainName = document.createElement("h1");
+    mountainData.innerHTML = ''; // clear previous mountain data
+    let mountainName = document.createElement('h1');
     mountainName.innerText = mountain.name;
-    mountainName.style = "display: inline; padding-inline-end: 0.3rem";
+    mountainName.style = 'display: inline; padding-inline-end: 0.3rem';
     mountainData.appendChild(mountainName);
 
-    let mountainElev = document.createElement("span");
+    let mountainElev = document.createElement('span');
     mountainElev.innerText = `- Elevation: ${mountain.elevation}`;
-    mountainElev.style = "color: rgba(0, 0, 0, 0.3)";
+    mountainElev.style = 'color: rgba(0, 0, 0, 0.3)';
     mountainData.appendChild(mountainElev);
 
-    let mountainEffort = document.createElement("h3");
+    let mountainEffort = document.createElement('h3');
     mountainEffort.innerText = `Effort Level: ${mountain.effort}`;
-    mountainEffort.style = "color: rgba(0, 0, 0, 0.7)";
+    mountainEffort.style = 'color: rgba(0, 0, 0, 0.7)';
     mountainData.appendChild(mountainEffort);
 
-    let mountainDescribe = document.createElement("div");
-    mountainDescribe.style = "display: flex; justify-content: space-between";
+    let mountainDescribe = document.createElement('div');
+    mountainDescribe.style = 'display: flex; justify-content: space-between';
     mountainData.appendChild(mountainDescribe);
-    let imgAndCoords = document.createElement("span");
+    let imgAndCoords = document.createElement('span');
     imgAndCoords.style =
-      "display: inherit; flex-direction: column; margin-inline-end: 2rem; text-align: center";
+      'display: inherit; flex-direction: column; margin-inline-end: 2rem; text-align: center';
     mountainDescribe.appendChild(imgAndCoords);
-    let mountainImg = document.createElement("img");
-    mountainImg.src = `images/${mountain.src}`;
-    mountainImg.style = "height: 400px; width: 100%; padding-bottom: 1rem";
+    let mountainImg = document.createElement('img');
+    mountainImg.src = `images/${mountain.img}`;
+    mountainImg.style = 'height: 400px; width: 100%; padding-bottom: 1rem';
     mountainImg.alt = mountain.name;
     imgAndCoords.appendChild(mountainImg);
-    let mountainCoords = document.createElement("span");
+    let mountainCoords = document.createElement('span');
     mountainCoords.innerText = `Latitude: ${mountain.coords.lat} Longitude: ${mountain.coords.lng}`;
-    mountainCoords.style = "text-align: center";
+    mountainCoords.style = 'text-align: center';
     imgAndCoords.appendChild(mountainCoords);
-    let mountainSunsetSunrise = document.createElement("div");
+    let mountainSunsetSunrise = document.createElement('div');
     imgAndCoords.appendChild(mountainSunsetSunrise);
 
-    let timeZone = "";
+    let timeZone = '';
 
     // Mountain Sunrise
-    let mountainSunrise = document.createElement("div");
+    let mountainSunrise = document.createElement('div');
     mountainSunrise.style =
-      "display: flex; flex-direction: row; justify-content: center";
+      'display: flex; flex-direction: row; justify-content: center';
     mountainSunsetSunrise.appendChild(mountainSunrise);
-    let mountainSunriseIcon = document.createElement("img");
-    mountainSunriseIcon.src = "images/sunrise.png";
-    mountainSunriseIcon.style = "height: 50px; width: 50px; display: inline";
+    let mountainSunriseIcon = document.createElement('img');
+    mountainSunriseIcon.src = 'images/sunrise.png';
+    mountainSunriseIcon.style = 'height: 50px; width: 50px; display: inline';
     mountainSunrise.appendChild(mountainSunriseIcon);
     // Display sunrise timezones with moment
-    let mountainSunriseTime = document.createElement("p");
-    mountainSunriseTime.className = "y-center";
+    let mountainSunriseTime = document.createElement('p');
+    mountainSunriseTime.className = 'y-center';
     mountainSunriseTime.innerText =
       (await displaySunriseTime(
         mountain.coords.lat,
         mountain.coords.lng,
         timeZone
-      )) + " (UTC)";
+      )) + ' (UTC)';
     mountainSunrise.appendChild(mountainSunriseTime);
     // Mountain Sunset
-    let mountainSunset = document.createElement("div");
+    let mountainSunset = document.createElement('div');
     mountainSunset.style =
-      "display: flex; flex-direction: row; justify-content: center";
+      'display: flex; flex-direction: row; justify-content: center';
     mountainSunsetSunrise.appendChild(mountainSunset);
-    let mountainSunsetIcon = document.createElement("img");
-    mountainSunsetIcon.src = "images/sunset.png";
-    mountainSunsetIcon.style = "height: 50px; width: 50px; display: inline";
+    let mountainSunsetIcon = document.createElement('img');
+    mountainSunsetIcon.src = 'images/sunset.png';
+    mountainSunsetIcon.style = 'height: 50px; width: 50px; display: inline';
     mountainSunset.appendChild(mountainSunsetIcon);
     // Display sunset timezones with moment
-    let mountainSunsetTime = document.createElement("p");
-    mountainSunsetTime.className = "y-center";
+    let mountainSunsetTime = document.createElement('p');
+    mountainSunsetTime.className = 'y-center';
     mountainSunsetTime.innerText =
       (await displaySunsetTime(
         mountain.coords.lat,
         mountain.coords.lng,
         timeZone
-      )) + " (UTC)";
+      )) + ' (UTC)';
     mountainSunset.appendChild(mountainSunsetTime);
 
-    let mountainDescription = document.createElement("span");
+    let mountainDescription = document.createElement('span');
     mountainDescription.innerText = mountain.desc;
-    mountainDescription.style = "font-size: 1.2rem; width: 40%";
+    mountainDescription.style = 'font-size: 1.2rem; width: 40%';
     mountainDescribe.appendChild(mountainDescription);
   }
 }
@@ -152,4 +152,7 @@ window.onload = async () => {
   await displayMountainsInSelect();
   displayMountainData();
 };
-mountainSelect.onchange = displayMountainData;
+mountainSelect.onchange = function () {
+  mountainData.style.display = 'block';
+  displayMountainData();
+};
